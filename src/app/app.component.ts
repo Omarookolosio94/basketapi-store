0b7afbea4128c5c6b8d4947e0f597e088dc38a9c
Oghenemaro Okolosio <Oghenemaro.Okolosio@stanbicibtc.com>
2022-05-22 21:48:37 +0100

    disabled log for production

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, isDevMode, OnInit } from '@angular/core';
 import { CartService } from './services/cart.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'basketapi-store';
 
-  constructor(private cartService: CartService){}
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
+    if (!isDevMode()) {
+      console.log = function () {};
+    }
+
     this.cartService.setCartCount();
-   }
+  }
 }
